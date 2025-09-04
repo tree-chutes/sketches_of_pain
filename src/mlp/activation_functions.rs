ede87c9d128ac6558086043737fe2770e20a3624
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-09-04 09:48:17 -0600

    Create activation_functions.rs

---
+++ src/mlp/activation_functions.rs
@@ -0,0 +1,22 @@
+//Copyright (c) 2025, tree-chutes
+
+use num_traits::Float;
+
+pub trait Activation<F: Float>{
+    fn calculate(&self, input: &mut F, bias: F);
+}
+
+
+pub fn activation_function_factory<F: Float + 'static>(s: F) -> Box<dyn Activation<F>>{
+    Box::new(Sigmoid{seed: s})
+}
+
+struct Sigmoid<F: Float>{
+    seed: F
+}
+
+impl<F: Float> Activation<F> for Sigmoid<F>{
+    fn calculate(&self, input: &mut F, b: F){
+        *input = self.seed/self.seed + (*input - b).exp(); 
+    }
+}
