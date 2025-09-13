fefbe19972dee7e7adad37a0bfd41ffb361ac708
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-09-13 04:28:56 -0600

    Update co5_dl_osx.c

--- src/c/co5_dl_osx.c
+++ src/c/co5_dl_osx.c
@@ -1,12 +1,8 @@
 //Copyright (c) 2025, tree-chutes
 
-#include <immintrin.h>
+const unsigned char REGISTER_WIDTH = 4;
 
-double* kernel;
-unsigned long output_shape;
-unsigned long weights_width;
-
-void multiply(double *in, double *out)
+unsigned char multiply(unsigned long counter, unsigned long trigger, unsigned long jumpback, double* in, double* kernel, double* out)
 {
     double* tmp1;
     __m256d operand1 = _mm256_setzero_pd();
@@ -14,33 +10,25 @@ void multiply(double *in, double *out)
     __m256d ret = _mm256_setzero_pd();
     double* tmp0 = in;
     double* tmp2 = out;
-
-    for(unsigned long c = 0; c < output_shape * weights_width; c++ )
+    
+    for(unsigned long c = 0; c < counter; c++ )
     {
-        if (c % weights_width == 0)
+        if (c % trigger == 0)
         {
             tmp1 = kernel;
+            if (c != 0)
+            {
+                tmp0 -= jumpback;
+                tmp2 -= jumpback;
+            }
         }
         operand1 = _mm256_loadu_pd(tmp0);
         operand2 = _mm256_loadu_pd(tmp1);
         ret = _mm256_mul_pd(operand1, operand2);
         _mm256_storeu_pd(tmp2, ret);
-        tmp0 += weights_width;
-        tmp1 += weights_width;
-        tmp2 += weights_width;
+        tmp0 += REGISTER_WIDTH;
+        tmp1 += REGISTER_WIDTH;
+        tmp2 += REGISTER_WIDTH;
     }
-}
-
-unsigned char init_kernel(unsigned long o, unsigned long w, double *k)
-{
-    output_shape = o;
-    weights_width = w;
-    kernel = k;
-    return 0;
-}
-
-unsigned char drop()
-{
-    free(kernel);
     return 0;
 }
