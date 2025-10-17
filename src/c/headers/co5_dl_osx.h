8ffb06b8d94cc41514279d8fa9e05adb367f33bc
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-17 16:18:42 -0600

    Add function prototype for padding float matrixPytorch/Colab match

--- src/c/headers/co5_dl_osx.h
+++ src/c/headers/co5_dl_osx.h
@@ -1,3 +1,4 @@
 // Copyright (c) 2025, tree-chutes
 
 double* pad_matrix_double(unsigned long h, unsigned long w, unsigned short p, double* m);
+float* pad_matrix_float(unsigned long h, unsigned long w, unsigned short p, float* m);
