061a92f8a71c3717a7703aa23d5b11be036e9042
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-17 16:26:35 -0600

    PyTorch/Colab match

--- src/mlp/loss_functions.rs
+++ src/mlp/loss_functions.rs
@@ -1,12 +1,32 @@
 //Copyright (c) 2025, tree-chutes
 
-use num_traits::Float;
-use super::cross_entropy::CrossEntropy;
+use core::panic;
 
-pub trait Loss<F: Float>{
-    fn calculate(&self, p: &[F], q: &[F])->F;
+use super::mean_squares::MeanSquares;
+use num_traits::Float;
+
+pub enum LossFunctions {
+    MeanSquares,
 }
 
-pub fn loss_function_factory<F: Float + 'static>(s: F) -> Box<dyn Loss<F>>{
-    Box::new(CrossEntropy{seed: s})
+pub trait Loss<F: Float> {
+    fn forward(&self, t: &[F], p: &[F]) -> Vec<F>;
+    fn backward(&self, loss: F, dl: &mut [F]) -> Vec<F>;
+    fn flatten(&mut self, mut _t: Vec<Vec<F>>) -> Vec<F> {
+        panic!()
+    }
+}
+
+pub fn loss_function_factory<F: Float + 'static>(
+    l: LossFunctions,
+    t: Vec<Vec<F>>,
+    s: F
+) -> (Vec<F>, Box<dyn Loss<F>>) {
+    match l {
+        LossFunctions::MeanSquares => {
+            let mut l1 = MeanSquares { one: s, n: 0, m: 0 };
+            (l1.flatten(t), Box::new(l1))
+        }
+        _ => todo!(),
+    }
 }
