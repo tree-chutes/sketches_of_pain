612c1cacc947aea10adc548d1e73b60a2f4ac3f9
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-26 14:27:11 -0600

    match

--- src/c/headers/co5_dl_osx.h
+++ src/c/headers/co5_dl_osx.h
@@ -1,4 +1,8 @@
 // Copyright (c) 2025, tree-chutes
 
+const unsigned char DOUBLE_REGISTER_COUNT = 8;
+const unsigned char FLOAT_REGISTER_COUNT = 16;
+
+
 double* pad_matrix_double(unsigned long h, unsigned long w, unsigned short p, double* m);
 float* pad_matrix_float(unsigned long h, unsigned long w, unsigned short p, float* m);
