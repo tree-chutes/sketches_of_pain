2e1ea4b86bda6091edf1551dd7e60c7128919ba9
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-26 14:24:21 -0600

    PyTorch match

--- src/mlp/activation_functions.rs
+++ src/mlp/activation_functions.rs
@@ -1,22 +1,23 @@
 //Copyright (c) 2025, tree-chutes
 
 use num_traits::Float;
+use super::softmax::Softmax;
+pub enum Activations {
+    Softmax
+}
+
 
 pub trait Activation<F: Float>{
-    fn calculate(&self, input: &mut F, bias: F);
+    fn forward(&self, input: &[F])-> Vec<F>;
+    fn backward(&self, truth: &[F], z: &[F]) -> Vec<F>;
 }
 
 
-pub fn activation_function_factory<F: Float + 'static>(s: F) -> Box<dyn Activation<F>>{
-    Box::new(Sigmoid{seed: s})
-}
-
-struct Sigmoid<F: Float>{
-    seed: F
-}
-
-impl<F: Float> Activation<F> for Sigmoid<F>{
-    fn calculate(&self, input: &mut F, b: F){
-        *input = self.seed/(self.seed + (-(*input + b)).exp()); 
+pub fn activation_function_factory<F: Float + 'static>(a: Activations, l: usize, s: F) -> Box<dyn Activation<F>>{
+    assert!(l != 0);
+    match a {
+        Activations::Softmax =>{
+            Box::new(Softmax::<F>{len: l, zero: s})
+        }
     }
 }
