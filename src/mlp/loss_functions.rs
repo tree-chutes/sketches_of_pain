054cacd25430068162b3a5e45aeddd8fc28aa2df
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-26 14:26:07 -0600

    match

--- src/mlp/loss_functions.rs
+++ src/mlp/loss_functions.rs
@@ -2,31 +2,35 @@
 
 use core::panic;
 
-use super::mean_squares::MeanSquares;
+use super::{cross_entropy::CrossEntropy, mean_squares::MeanSquares};
 use num_traits::Float;
 
 pub enum LossFunctions {
     MeanSquares,
+    CrossEntropy
 }
 
 pub trait Loss<F: Float> {
     fn forward(&self, t: &[F], p: &[F]) -> Vec<F>;
-    fn backward(&self, loss: F, dl: &mut [F]) -> Vec<F>;
-    fn flatten(&mut self, mut _t: Vec<Vec<F>>) -> Vec<F> {
+    fn backward(&self, loss: &[F], dl: &mut [F]) -> Vec<F>;
+    fn resize(&self, _t: Vec<F>) -> Vec<F> {
         panic!()
     }
 }
 
 pub fn loss_function_factory<F: Float + 'static>(
     l: LossFunctions,
-    t: Vec<Vec<F>>,
+    t: Vec<F>,
     s: F
 ) -> (Vec<F>, Box<dyn Loss<F>>) {
     match l {
         LossFunctions::MeanSquares => {
-            let mut l1 = MeanSquares { one: s, n: 0, m: 0 };
-            (l1.flatten(t), Box::new(l1))
+            let mut l1 = MeanSquares { one: s, len: t.len() };
+            (l1.resize(t), Box::new(l1))
         }
-        _ => todo!(),
+        LossFunctions::CrossEntropy => {
+            let mut l1 = CrossEntropy{len: t.len(), zero: s}; 
+            (l1.resize(t), Box::new(l1))
+        }
     }
 }
