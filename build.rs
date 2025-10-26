0597237673a8f139a60abe86edd0ba9f05ecd6e6
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-26 14:32:25 -0600

    Update build.rs for new source files and flags

--- build.rs
+++ build.rs
@@ -2,18 +2,28 @@
 
 #[cfg(target_os = "linux")]
 fn main() {
-    println!("cargo:rerun-if-changed=./src/c/co5_dl_osx.c");
+    println!("cargo:rerun-if-changed=../core/src/c/co5_lib.c");
     cc::Build::new()
-                .files(vec!["../core/src/c/co5_dl_osx.c"])
-                .compile("co5_dl_c");
+                .files(vec!["../core/src/c/co5_lib.c"])
+                .include("../core/src/c/headers")
+                .compile("co5_c_lib");
 }
 
 #[cfg(not(target_os = "linux"))]
 fn main() {
-    println!("cargo:rerun-if-changed=./src/c/co5_dl_osx.c");
+    let source_files = vec!["./src/c/co5_dl_double_osx.c",
+        "./src/c/co5_dl_losses_double_osx.c",
+        "./src/c/co5_dl_float_osx.c",
+        "./src/c/co5_dl_losses_float_osx.c"];
+    println!("cargo:rerun-if-changed=./src/c/*");
     cc::Build::new()
-                .files(vec!["./src/c/co5_dl_osx.c"])
+                .files(source_files)
+                .include("./src/c/headers")
                 .flag("-w")
+                .flag("-g")
+                //.flag("-fsanitize=address")
                 .flag("-mavx")
+                .flag("-mfma")
+                .flag("-mavx512f")
                 .compile("co5_dl_c");
 }
