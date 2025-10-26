db03afa251d591a8b06f83ca5f487a08b2d5c3e9
tree-chutes <17575874+tree-chutes@users.noreply.github.com>
2025-10-26 14:24:57 -0600

    match
    Added resizing and forward methods to CrossEntropy struct.

--- src/mlp/cross_entropy.rs
+++ src/mlp/cross_entropy.rs
@@ -1,21 +1,51 @@
 //Copyright (c) 2025, tree-chutes
 
-use num_traits::Float;
 use super::loss_functions::Loss;
+use super::register::REGISTER_WIDTH;
+use num_traits::Float;
 
-pub(in super) struct CrossEntropy<F: Float>{
-    pub (in super) seed: F,
+pub(super) struct CrossEntropy<F: Float> {
+    pub(super) len: usize,
+    pub(super) zero: F,
 }
 
-impl<F: Float> Loss<F> for CrossEntropy<F>{
-    fn calculate(&self, p: &[F], q: &[F])-> F{
-        let mut ret: F = self.seed;
-        if p.len() != q.len(){
-            panic!("p shape is {}, q shape is {}", p.len(), q.len());
+impl<F: Float> Loss<F> for CrossEntropy<F> {
+    fn resize(&self, t: Vec<F>) -> Vec<F> {
+        assert!(t.len() == self.len);
+        let mut ret: Vec<F> = t;
+        ret.resize(
+            ret.len() + (REGISTER_WIDTH / (size_of::<F>() * 8))
+                - ret.len() % (REGISTER_WIDTH / (size_of::<F>() * 8)),
+            self.zero,
+        );
+        ret
+    }
+
+    fn forward(&self, t: &[F], p: &[F]) -> Vec<F> {
+        let truth_len: usize = self.len + (REGISTER_WIDTH / (size_of::<F>() * 8))
+            - self.len % (REGISTER_WIDTH / (size_of::<F>() * 8));
+
+        assert!(t.len() == truth_len);
+        assert!(p.len() == truth_len);
+        self.execute_forward(t, p)
+    }
+
+    fn backward(&self, t: &[F], p: &mut [F]) -> Vec<F> {
+        vec![]
+    }
+}
+
+impl<F:Float> CrossEntropy<F>{
+    fn execute_forward(&self, t: &[F], p: &[F]) -> Vec<F>{
+        let mut ret: Vec<F> = vec![self.zero; 1];
+        let mut idx: usize = 0;
+        loop{
+            if t[idx] != self.zero{
+                ret[0] = -p[idx].ln();
+                break;
+            }
+            idx += 1;
         }
-        for i in 0..(p.len()){
-            ret = ret + p[i] * q[i].log2();
-        }
-        -ret
+        ret
     }
 }
